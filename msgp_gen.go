32c1c0704210479797091edc1e4ca1a9d74e3bc1
philhofer <phofer@umich.edu>
2014-10-21 10:37:25 -0400

    new msgp unmarshal implementation

--- msgp_gen.go
+++ msgp_gen.go
@@ -5,226 +5,293 @@ package goserbench
 // DO NOT EDIT
 
 import (
+	"bytes"
 	"github.com/philhofer/msgp/enc"
 	"io"
-	"bytes"
 )
 
-func (z *A) Marshal() ([]byte, error) {
+// MarshalMsg marshals a A into MessagePack
+func (z *A) MarshalMsg() ([]byte, error) {
 	var buf bytes.Buffer
-	_, err := z.WriteTo(&buf)
+	_, err := z.EncodeMsg(&buf)
 	return buf.Bytes(), err
 }
 
-func (z *A) Unmarshal(b []byte) error {
-	_, err := z.ReadFrom(bytes.NewReader(b))
-	return err
+// UnmarshalMsg unmarshals a A from MessagePack, returning any extra bytes
+// and any errors encountered
+func (z *A) UnmarshalMsg(bts []byte) (o []byte, err error) {
+	var sz uint32
+	var field []byte
+	_ = sz
+	_ = field
+
+	var isz uint32
+	isz, bts, err = enc.ReadMapHeaderBytes(bts)
+	if err != nil {
+		return
+	}
+	for xplz := uint32(0); xplz < isz; xplz++ {
+		field, bts, err = enc.ReadStringZC(bts)
+		if err != nil {
+			return
+		}
+		switch enc.UnsafeString(field) {
+
+		case "Name":
+
+			z.Name, bts, err = enc.ReadStringBytes(bts)
+
+			if err != nil {
+				return
+			}
+
+		case "BirthDay":
+
+			z.BirthDay, bts, err = enc.ReadTimeBytes(bts)
+
+			if err != nil {
+				return
+			}
+
+		case "Phone":
+
+			z.Phone, bts, err = enc.ReadStringBytes(bts)
+
+			if err != nil {
+				return
+			}
+
+		case "Siblings":
+
+			z.Siblings, bts, err = enc.ReadIntBytes(bts)
+
+			if err != nil {
+				return
+			}
+
+		case "Spouse":
+
+			z.Spouse, bts, err = enc.ReadBoolBytes(bts)
+
+			if err != nil {
+				return
+			}
+
+		case "Money":
+
+			z.Money, bts, err = enc.ReadFloat64Bytes(bts)
+
+			if err != nil {
+				return
+			}
+
+		default:
+			bts, err = enc.Skip(bts)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	o = bts
+	return
 }
 
-func (z *A) WriteTo(w io.Writer) (n int64, err error) {
-	var nn int
+// EncodeMsg encodes a A as MessagePack to the supplied io.Writer,
+// returning the number of bytes written and any errors encountered
+func (z *A) EncodeMsg(w io.Writer) (n int, err error) {
 	en := enc.NewEncoder(w)
+	return z.EncodeTo(en)
+}
+
+// EncodeTo encodes a A as MessagePack using the provided encoder,
+// returning the number of bytes written and any errors encountered
+func (z *A) EncodeTo(en *enc.MsgWriter) (n int, err error) {
+	var nn int
 	_ = nn
-	_ = en
 
-	if z == nil {
-		nn, err = en.WriteNil()
-		n += int64(nn)
-		if err != nil {
-			return
-		}
-	} else {
+	nn, err = en.WriteMapHeader(6)
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteMapHeader(uint32(6))
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString("Name")
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteString("Name")
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString(z.Name)
 
-		nn, err = en.WriteString(z.Name)
+	n += nn
+	if err != nil {
+		return
+	}
 
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString("BirthDay")
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteString("BirthDay")
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteTime(z.BirthDay)
 
-		nn, err = en.WriteTime(z.BirthDay)
+	n += nn
+	if err != nil {
+		return
+	}
 
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString("Phone")
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteString("Phone")
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString(z.Phone)
 
-		nn, err = en.WriteString(z.Phone)
+	n += nn
+	if err != nil {
+		return
+	}
 
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString("Siblings")
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteString("Siblings")
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteInt(z.Siblings)
 
-		nn, err = en.WriteInt(z.Siblings)
+	n += nn
+	if err != nil {
+		return
+	}
 
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString("Spouse")
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteString("Spouse")
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteBool(z.Spouse)
 
-		nn, err = en.WriteBool(z.Spouse)
+	n += nn
+	if err != nil {
+		return
+	}
 
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteString("Money")
+	n += nn
+	if err != nil {
+		return
+	}
 
-		nn, err = en.WriteString("Money")
-		n += int64(nn)
-		if err != nil {
-			return
-		}
+	nn, err = en.WriteFloat64(z.Money)
 
-		nn, err = en.WriteFloat64(z.Money)
-
-		n += int64(nn)
-		if err != nil {
-			return
-		}
-
+	n += nn
+	if err != nil {
+		return
 	}
 
 	return
 }
 
-func (z *A) ReadFrom(r io.Reader) (n int64, err error) {
-	var sz uint32
-	var nn int
-	var field []byte
+// DecodeMsg decodes MessagePack from the provided io.Reader into the A,
+// returning the number of bytes read and any errors encountered
+func (z *A) DecodeMsg(r io.Reader) (n int, err error) {
 	dc := enc.NewDecoder(r)
-	_ = sz
-	_ = nn
-	_ = field
-
-	if dc.IsNil() {
-		nn, err = dc.ReadNil()
-		n += int64(nn)
-		if err != nil {
-			return
-		}
-		z = nil
-	} else {
-		if z == nil {
-			z = new(A)
-		}
-
-		var isz uint32
-		isz, nn, err = dc.ReadMapHeader()
-		n += int64(nn)
-		if err != nil {
-			return
-		}
-		for xplz := uint32(0); xplz < isz; xplz++ {
-			field, nn, err = dc.ReadStringAsBytes(field)
-			n += int64(nn)
-			if err != nil {
-				return
-			}
-			switch enc.UnsafeString(field) {
-
-			case "Name":
-
-				z.Name, nn, err = dc.ReadString()
-
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-
-			case "BirthDay":
-
-				z.BirthDay, nn, err = dc.ReadTime()
-
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-
-			case "Phone":
-
-				z.Phone, nn, err = dc.ReadString()
-
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-
-			case "Siblings":
-
-				z.Siblings, nn, err = dc.ReadInt()
-
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-
-			case "Spouse":
-
-				z.Spouse, nn, err = dc.ReadBool()
-
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-
-			case "Money":
-
-				z.Money, nn, err = dc.ReadFloat64()
-
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-
-			default:
-				nn, err = dc.Skip()
-				n += int64(nn)
-				if err != nil {
-					return
-				}
-			}
-		}
-
-	}
-
+	n, err = z.DecodeFrom(dc)
 	enc.Done(dc)
 	return
 }
 
+// DecodeFrom deocdes MessagePack from the provided decoder into the A,
+// returning the number of bytes read and any errors encountered.
+func (z *A) DecodeFrom(dc *enc.MsgReader) (n int, err error) {
+	var sz uint32
+	var nn int
+	var field []byte
+	_ = sz
+	_ = nn
+	_ = field
+
+	var isz uint32
+	isz, nn, err = dc.ReadMapHeader()
+	n += nn
+	if err != nil {
+		return
+	}
+	for xplz := uint32(0); xplz < isz; xplz++ {
+		field, nn, err = dc.ReadStringAsBytes(field)
+		n += nn
+		if err != nil {
+			return
+		}
+		switch enc.UnsafeString(field) {
+
+		case "Name":
+
+			z.Name, nn, err = dc.ReadString()
+
+			n += nn
+			if err != nil {
+				return
+			}
+
+		case "BirthDay":
+
+			z.BirthDay, nn, err = dc.ReadTime()
+
+			n += nn
+			if err != nil {
+				return
+			}
+
+		case "Phone":
+
+			z.Phone, nn, err = dc.ReadString()
+
+			n += nn
+			if err != nil {
+				return
+			}
+
+		case "Siblings":
+
+			z.Siblings, nn, err = dc.ReadInt()
+
+			n += nn
+			if err != nil {
+				return
+			}
+
+		case "Spouse":
+
+			z.Spouse, nn, err = dc.ReadBool()
+
+			n += nn
+			if err != nil {
+				return
+			}
+
+		case "Money":
+
+			z.Money, nn, err = dc.ReadFloat64()
+
+			n += nn
+			if err != nil {
+				return
+			}
+
+		default:
+			nn, err = dc.Skip()
+			n += nn
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
