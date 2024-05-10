7f50727b6b13bc37e0827040b21fee0cc281476d
Matheus Degiovani <opensource@matheusd.com>
2024-05-10 14:14:26 -0300

    gencode: Switch to serializer tests

--- serialization_benchmarks_test.go
+++ serialization_benchmarks_test.go
@@ -1126,134 +1126,20 @@ func Benchmark_Colfer_Unmarshal(b *testing.B) {
 
 // github.com/andyleap/gencode
 
-func generateGencode() []*GencodeA {
-	a := make([]*GencodeA, 0, 1000)
-	for i := 0; i < 1000; i++ {
-		a = append(a, &GencodeA{
-			Name:     randString(16),
-			BirthDay: time.Now(),
-			Phone:    randString(10),
-			Siblings: rand.Int31n(5),
-			Spouse:   rand.Intn(2) == 1,
-			Money:    rand.Float64(),
-		})
-	}
-	return a
-}
-
 func Benchmark_Gencode_Marshal(b *testing.B) {
-	data := generateGencode()
-	b.ReportAllocs()
-	b.ResetTimer()
-	var serialSize int
-	for i := 0; i < b.N; i++ {
-		bytes, err := data[rand.Intn(len(data))].Marshal(nil)
-		if err != nil {
-			b.Fatal(err)
-		}
-		serialSize += len(bytes)
-	}
-	b.ReportMetric(float64(serialSize)/float64(b.N), "B/serial")
+	benchMarshal(b, newGencodeSerializer())
 }
 
 func Benchmark_Gencode_Unmarshal(b *testing.B) {
-	b.StopTimer()
-	data := generateGencode()
-	ser := make([][]byte, len(data))
-	var serialSize int
-	for i, d := range data {
-		var err error
-		ser[i], err = d.Marshal(nil)
-		if err != nil {
-			b.Fatal(err)
-		}
-		serialSize += len(ser[i])
-	}
-	b.ReportMetric(float64(serialSize)/float64(len(data)), "B/serial")
-	b.ReportAllocs()
-	b.StartTimer()
-
-	for i := 0; i < b.N; i++ {
-		n := rand.Intn(len(ser))
-		o := &GencodeA{}
-		_, err := o.Unmarshal(ser[n])
-		if err != nil {
-			b.Fatalf("gencode failed to unmarshal: %s (%s)", err, ser[n])
-		}
-		// Validate unmarshalled data.
-		if validate != "" {
-			i := data[n]
-			correct := o.Name == i.Name && o.Phone == i.Phone && o.Siblings == i.Siblings && o.Spouse == i.Spouse && o.Money == i.Money && o.BirthDay.Equal(i.BirthDay) //&& cmpTags(o.Tags, i.Tags) && cmpAliases(o.Aliases, i.Aliases)
-			if !correct {
-				b.Fatalf("unmarshaled object differed:\n%v\n%v", i, o)
-			}
-		}
-	}
-}
-
-func generateGencodeUnsafe() []*GencodeUnsafeA {
-	a := make([]*GencodeUnsafeA, 0, 1000)
-	for i := 0; i < 1000; i++ {
-		a = append(a, &GencodeUnsafeA{
-			Name:     randString(16),
-			BirthDay: time.Now().UnixNano(),
-			Phone:    randString(10),
-			Siblings: rand.Int31n(5),
-			Spouse:   rand.Intn(2) == 1,
-			Money:    rand.Float64(),
-		})
-	}
-	return a
+	benchUnmarshal(b, newGencodeSerializer())
 }
 
 func Benchmark_GencodeUnsafe_Marshal(b *testing.B) {
-	data := generateGencodeUnsafe()
-	b.ReportAllocs()
-	b.ResetTimer()
-	var serialSize int
-	for i := 0; i < b.N; i++ {
-		bytes, err := data[rand.Intn(len(data))].Marshal(nil)
-		if err != nil {
-			b.Fatal(err)
-		}
-		serialSize += len(bytes)
-	}
-	b.ReportMetric(float64(serialSize)/float64(b.N), "B/serial")
+	benchMarshal(b, newGencodeUnsafeSerializer())
 }
 
 func Benchmark_GencodeUnsafe_Unmarshal(b *testing.B) {
-	b.StopTimer()
-	data := generateGencodeUnsafe()
-	ser := make([][]byte, len(data))
-	var serialSize int
-	for i, d := range data {
-		var err error
-		ser[i], err = d.Marshal(nil)
-		if err != nil {
-			b.Fatal(err)
-		}
-		serialSize += len(ser[i])
-	}
-	b.ReportMetric(float64(serialSize)/float64(len(data)), "B/serial")
-	b.ReportAllocs()
-	b.StartTimer()
-
-	for i := 0; i < b.N; i++ {
-		n := rand.Intn(len(ser))
-		o := &GencodeUnsafeA{}
-		_, err := o.Unmarshal(ser[n])
-		if err != nil {
-			b.Fatalf("gencode failed to unmarshal: %s (%s)", err, ser[n])
-		}
-		// Validate unmarshalled data.
-		if validate != "" {
-			i := data[n]
-			correct := o.Name == i.Name && o.Phone == i.Phone && o.Siblings == i.Siblings && o.Spouse == i.Spouse && o.Money == i.Money && o.BirthDay == i.BirthDay //&& cmpTags(o.Tags, i.Tags) && cmpAliases(o.Aliases, i.Aliases)
-			if !correct {
-				b.Fatalf("unmarshaled object differed:\n%v\n%v", i, o)
-			}
-		}
-	}
+	benchUnmarshal(b, newGencodeUnsafeSerializer())
 }
 
 // github.com/calmh/xdr
