cfac10bb3f9c9e52b9dece8d4c11c58ad1100221
Matheus Degiovani <opensource@matheusd.com>
2024-05-16 09:28:18 -0300

    capnproto: Move to own file

--- serialization_benchmarks_test.go
+++ serialization_benchmarks_test.go
@@ -15,7 +15,6 @@ import (
 	"github.com/Sereal/Sereal/Go/sereal"
 	"github.com/alecthomas/binary"
 	"github.com/davecgh/go-xdr/xdr"
-	capn "github.com/glycerine/go-capnproto"
 	flatbuffers "github.com/google/flatbuffers/go"
 	jsoniter "github.com/json-iterator/go"
 	easyjson "github.com/mailru/easyjson"
@@ -563,45 +562,12 @@ func Benchmark_FlatBuffers_Unmarshal(b *testing.B) {
 
 // github.com/glycerine/go-capnproto
 
-type CapNProtoSerializer struct{}
-
-func (x *CapNProtoSerializer) Marshal(o interface{}) ([]byte, error) {
-	a := o.(*A)
-	seg := capn.NewBuffer(nil)
-	c := NewRootCapnpA(seg)
-	c.SetName(a.Name)
-	c.SetBirthDay(a.BirthDay.UnixNano())
-	c.SetPhone(a.Phone)
-	c.SetSiblings(int32(a.Siblings))
-	c.SetSpouse(a.Spouse)
-	c.SetMoney(a.Money)
-	var buf bytes.Buffer
-	_, err := seg.WriteTo(&buf)
-	return buf.Bytes(), err
-}
-
-func (x *CapNProtoSerializer) Unmarshal(d []byte, i interface{}) error {
-	a := i.(*A)
-	s, _, err := capn.ReadFromMemoryZeroCopy(d)
-	if err != nil {
-		return err
-	}
-	o := ReadRootCapnpA(s)
-	a.Name = o.Name()
-	a.BirthDay = time.Unix(0, o.BirthDay())
-	a.Phone = o.Phone()
-	a.Siblings = int(o.Siblings())
-	a.Spouse = o.Spouse()
-	a.Money = o.Money()
-	return nil
-}
-
 func Benchmark_CapNProto_Marshal(b *testing.B) {
-	benchMarshal(b, &CapNProtoSerializer{})
+	benchMarshal(b, newCapNProtoSerializer())
 }
 
 func Benchmark_CapNProto_Unmarshal(b *testing.B) {
-	benchUnmarshal(b, &CapNProtoSerializer{})
+	benchUnmarshal(b, newCapNProtoSerializer())
 }
 
 // github.com/hprose/hprose-go/io
