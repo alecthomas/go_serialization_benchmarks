8ed5168cf8d89ad1f27a345102daeba3b17b109e
yra <ymz.ncnk@outlook.com>
2024-06-25 17:05:51 +0300

    Update MUS to work like Bebop

--- internal/serializers/mus/mus.go
+++ internal/serializers/mus/mus.go
@@ -5,43 +5,45 @@ import (
 
 	"github.com/alecthomas/go_serialization_benchmarks/goserbench"
 	"github.com/mus-format/mus-go/ord"
-	"github.com/mus-format/mus-go/raw"
 	"github.com/mus-format/mus-go/unsafe"
-	"github.com/mus-format/mus-go/varint"
 )
 
 type MUSSerializer struct{}
 
 func (s MUSSerializer) Marshal(o interface{}) ([]byte, error) {
-	v := o.(*goserbench.SmallStruct)
+	var (
+		v    = o.(*goserbench.SmallStruct)
+		nano = v.BirthDay.UnixNano()
+	)
 	n := ord.SizeString(v.Name)
-	n += raw.SizeInt64(v.BirthDay.UnixNano())
+	n += unsafe.SizeInt64(nano)
 	n += ord.SizeString(v.Phone)
-	n += varint.SizeInt32(int32(v.Siblings))
-	n += ord.SizeBool(v.Spouse)
-	n += raw.SizeFloat64(v.Money)
+	n += unsafe.SizeInt32(int32(v.Siblings))
+	n += unsafe.SizeBool(v.Spouse)
+	n += unsafe.SizeFloat64(v.Money)
 	buf := make([]byte, n)
 	n = ord.MarshalString(v.Name, buf)
-	n += raw.MarshalInt64(v.BirthDay.UnixNano(), buf[n:])
+	n += unsafe.MarshalInt64(nano, buf[n:])
 	n += ord.MarshalString(v.Phone, buf[n:])
-	n += varint.MarshalInt32(int32(v.Siblings), buf[n:])
-	n += ord.MarshalBool(v.Spouse, buf[n:])
-	raw.MarshalFloat64(v.Money, buf[n:])
+	n += unsafe.MarshalInt32(int32(v.Siblings), buf[n:])
+	n += unsafe.MarshalBool(v.Spouse, buf[n:])
+	unsafe.MarshalFloat64(v.Money, buf[n:])
 	return buf, nil
 }
 
 func (s MUSSerializer) Unmarshal(bs []byte, o interface{}) (err error) {
-	v := o.(*goserbench.SmallStruct)
-	var n int
+	var (
+		v          = o.(*goserbench.SmallStruct)
+		n          int
+		n1         int
+		birthDay64 int64
+		siblings32 int32
+	)
 	v.Name, n, err = ord.UnmarshalString(bs)
 	if err != nil {
 		return
 	}
-	var (
-		n1         int
-		birthDay64 int64
-	)
-	birthDay64, n1, err = raw.UnmarshalInt64(bs[n:])
+	birthDay64, n1, err = unsafe.UnmarshalInt64(bs[n:])
 	n += n1
 	if err != nil {
 		return
@@ -52,19 +54,18 @@ func (s MUSSerializer) Unmarshal(bs []byte, o interface{}) (err error) {
 	if err != nil {
 		return
 	}
-	var siblings32 int32
-	siblings32, n1, err = varint.UnmarshalInt32(bs[n:])
+	siblings32, n1, err = unsafe.UnmarshalInt32(bs[n:])
 	n += n1
 	if err != nil {
 		return
 	}
 	v.Siblings = int(siblings32)
-	v.Spouse, n1, err = ord.UnmarshalBool(bs[n:])
+	v.Spouse, n1, err = unsafe.UnmarshalBool(bs[n:])
 	n += n1
 	if err != nil {
 		return
 	}
-	v.Money, n1, err = raw.UnmarshalFloat64(bs[n:])
+	v.Money, n1, err = unsafe.UnmarshalFloat64(bs[n:])
 	n += n1
 	return
 }
