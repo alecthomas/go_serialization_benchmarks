73ff624c4ba0612feaec0b8b3fd4ce287fc12d78
Matheus Degiovani <opensource@matheusd.com>
2025-07-03 09:04:20 -0300

    flatbuffers: Add unsafe_reuse benchmark

--- internal/serializers/flatbuffers/flatbuffers.go
+++ internal/serializers/flatbuffers/flatbuffers.go
@@ -2,19 +2,23 @@ package flatbuffers
 
 import (
 	"time"
+	"unsafe"
 
 	"github.com/alecthomas/go_serialization_benchmarks/goserbench"
 	flatbuffers "github.com/google/flatbuffers/go"
 )
 
 type FlatBufferSerializer struct {
-	builder *flatbuffers.Builder
+	unsafeReuse bool
+	builder     *flatbuffers.Builder
 }
 
 func (s *FlatBufferSerializer) Marshal(o interface{}) ([]byte, error) {
 	a := o.(*goserbench.SmallStruct)
 	builder := s.builder
-	builder.Bytes = nil // free
+	if !s.unsafeReuse {
+		builder.Bytes = nil // free
+	}
 	builder.Reset()
 
 	name := builder.CreateString(a.Name)
@@ -28,16 +32,21 @@ func (s *FlatBufferSerializer) Marshal(o interface{}) ([]byte, error) {
 	FlatBufferAAddSpouse(builder, a.Spouse)
 	FlatBufferAAddMoney(builder, a.Money)
 	builder.Finish(FlatBufferAEnd(builder))
-	return builder.Bytes[builder.Head():], nil
+	return builder.FinishedBytes(), nil
 }
 
 func (s *FlatBufferSerializer) Unmarshal(d []byte, i interface{}) error {
 	a := i.(*goserbench.SmallStruct)
 	o := FlatBufferA{}
 	o.Init(d, flatbuffers.GetUOffsetT(d))
-	a.Name = string(o.Name())
+	if s.unsafeReuse {
+		a.Name = unsafeSliceToString(o.Name())
+		a.Phone = unsafeSliceToString(o.Phone())
+	} else {
+		a.Name = string(o.Name())
+		a.Phone = string(o.Phone())
+	}
 	a.BirthDay = time.Unix(0, o.BirthDay())
-	a.Phone = string(o.Phone())
 	a.Siblings = int(o.Siblings())
 	a.Spouse = o.Spouse()
 	a.Money = o.Money()
@@ -45,5 +54,16 @@ func (s *FlatBufferSerializer) Unmarshal(d []byte, i interface{}) error {
 }
 
 func NewFlatBuffersSerializer() goserbench.Serializer {
-	return &FlatBufferSerializer{flatbuffers.NewBuilder(0)}
+	return &FlatBufferSerializer{builder: flatbuffers.NewBuilder(0), unsafeReuse: false}
+}
+
+func NewFlatBuffersUnsafeReuseSerializer() goserbench.Serializer {
+	const maxSerSize = 128
+	return &FlatBufferSerializer{builder: flatbuffers.NewBuilder(maxSerSize), unsafeReuse: true}
+}
+
+// unsafeSliceToString converts a byte slice to a string in an unsafe way
+// (modifications to the byte slice modify the string).
+func unsafeSliceToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
